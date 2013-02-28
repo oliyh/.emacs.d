f286511d741c06ba707eac08507456a6564a8d9e
Martin Trojer <martin.trojer@gmail.com>
2013-02-28 19:16:06 +0000

    updated packages

--- elpa/nrepl-ritz-20130210.1550/nrepl-ritz-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "nrepl-ritz" "20130210.1550" "nrepl extensions for ritz [source: github]" (quote ((nrepl "0.1.6"))))
---
+++ elpa/nrepl-ritz-20130224.2047/nrepl-ritz-pkg.el
@@ -0,0 +1,1 @@
+(define-package "nrepl-ritz" "20130224.2047" "nrepl extensions for ritz [source: github]" (quote ((nrepl "0.1.6") (fringe-helper "0.1.1"))))
